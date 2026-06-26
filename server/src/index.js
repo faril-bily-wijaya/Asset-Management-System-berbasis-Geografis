44ed7a17bc4fb627c253e2831ad6b709024c59e8
faril-bily-wijaya <farilbilywijaya22@gmail.com>
2026-06-26 11:10:35 +0700

    Fix helmet forcing HTTPS on IP

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middleware
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: false,
+    crossOriginOpenerPolicy: false
+}));
 app.use(cors({
     origin: process.env.FRONTEND_URL || '*',
     credentials: true
